b8fd247275dd202f8e0f865ea3bd117bd98df315
alexander.korovin <alexander.korovin@vk.team>
2025-03-14 17:44:26 +0300

    Import Custom OpenAI settings from file

--- src/main/kotlin/ee/carlrobert/codegpt/settings/service/custom/form/CustomSettingsFileProvider.kt
+++ src/main/kotlin/ee/carlrobert/codegpt/settings/service/custom/form/CustomSettingsFileProvider.kt
@@ -1,11 +1,15 @@
 package ee.carlrobert.codegpt.settings.service.custom.form
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
 import ee.carlrobert.codegpt.settings.service.custom.form.model.CustomServiceSettingsData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
 import java.io.FileWriter
 
 class CustomSettingsFileProvider {
@@ -13,6 +17,8 @@ class CustomSettingsFileProvider {
     private val objectMapper: ObjectMapper = ObjectMapper()
         .registerModule(Jdk8Module())
         .registerModule(JavaTimeModule())
+        .registerModule(KotlinModule.Builder().build())
+        .apply { configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false) }
 
     suspend fun writeSettings(path: String, data: List<CustomServiceSettingsData>) {
         withContext(Dispatchers.IO) {
@@ -24,4 +30,7 @@ class CustomSettingsFileProvider {
             }
         }
     }
+
+    fun readFromFile(path: String): List<CustomServiceSettingsData> =
+        objectMapper.readValue<List<CustomServiceSettingsData>>(File(path))
 }
