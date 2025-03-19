654951d59c85cca9061bb64304289fb54865f76f
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2025-03-19 23:47:33 +0000

    feat: improve chat tags UX

--- src/main/java/ee/carlrobert/codegpt/toolwindow/chat/ChatToolWindowTabPanel.java
+++ src/main/java/ee/carlrobert/codegpt/toolwindow/chat/ChatToolWindowTabPanel.java
@@ -97,7 +97,6 @@ public class ChatToolWindowTabPanel implements Disposable {
     );
 
     totalTokensPanel = new TotalTokensPanel(
-        project,
         conversation,
         EditorUtil.getSelectedEditorSelectedText(project),
         this,
@@ -231,7 +230,8 @@ public class ChatToolWindowTabPanel implements Disposable {
 
       totalTokensPanel.updateConversationTokens(conversation);
       if (callParameters.getReferencedFiles() != null) {
-        totalTokensPanel.updateReferencedFilesTokens(callParameters.getReferencedFiles());
+        totalTokensPanel.updateReferencedFilesTokens(
+            callParameters.getReferencedFiles().stream().map(ReferencedFile::fileContent).toList());
       }
 
       var userMessagePanel = createUserMessagePanel(message, callParameters);
@@ -243,6 +243,12 @@ public class ChatToolWindowTabPanel implements Disposable {
 
       call(callParameters, responseMessagePanel, userMessagePanel);
     });
+  }
+
+  public void includeFiles(List<VirtualFile> referencedFiles) {
+    userInputPanel.includeFiles(referencedFiles);
+    totalTokensPanel.updateReferencedFilesTokens(
+        referencedFiles.stream().map(it -> ReferencedFile.from(it).fileContent()).toList());
   }
 
   private boolean hasReferencedFilePaths(Message message) {
@@ -281,8 +287,9 @@ public class ChatToolWindowTabPanel implements Disposable {
     return panel;
   }
 
-  private void reloadMessage(ChatCompletionParameters prevParameters,
-                             UserMessagePanel userMessagePanel) {
+  private void reloadMessage(
+      ChatCompletionParameters prevParameters,
+      UserMessagePanel userMessagePanel) {
     var prevMessage = prevParameters.getMessage();
     ResponseMessagePanel responsePanel = null;
     try {
