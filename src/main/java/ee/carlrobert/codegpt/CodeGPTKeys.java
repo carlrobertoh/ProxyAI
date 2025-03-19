36d042c6a5f58752ba7fbc1c935340e4982d393b
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2025-03-19 01:59:23 +0000

    fix: temp disable code completions when next edits is active

--- src/main/java/ee/carlrobert/codegpt/CodeGPTKeys.java
+++ src/main/java/ee/carlrobert/codegpt/CodeGPTKeys.java
@@ -5,7 +5,6 @@ import com.intellij.openapi.editor.Inlay;
 import com.intellij.openapi.util.Key;
 import ee.carlrobert.codegpt.predictions.CodeSuggestionDiffViewer;
 import ee.carlrobert.llm.client.codegpt.CodeGPTUserDetails;
-import okhttp3.Call;
 
 public class CodeGPTKeys {
 
@@ -19,10 +18,12 @@ public class CodeGPTKeys {
       Key.create("codegpt.imageAttachmentFilePath");
   public static final Key<CodeGPTUserDetails> CODEGPT_USER_DETAILS =
       Key.create("codegpt.userDetails");
+  public static final Key<String> REMAINING_EDITOR_COMPLETION =
+      Key.create("codegpt.editorCompletionLines");
+  public static final Key<Boolean> COMPLETION_IN_PROGRESS =
+      Key.create("codegpt.completionInProgress");
   public static final Key<Boolean> IS_PROMPT_TEXT_FIELD_DOCUMENT =
       Key.create("codegpt.isPromptTextFieldDocument");
   public static final Key<CodeSuggestionDiffViewer> EDITOR_PREDICTION_DIFF_VIEWER =
       Key.create("codegpt.editorPredictionDiffViewer");
-  public static final Key<Call> PENDING_PREDICTION_CALL =
-      Key.create("codegpt.editorPendingPredictionCall");
 }
