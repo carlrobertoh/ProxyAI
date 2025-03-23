918d93a9c24b715082bc09530adf24cb2c6175b6
alexander.korovin <alexander.korovin@vk.team>
2025-03-23 15:33:23 +0300

    Added Coroutine dispatchers for EDT

--- src/main/kotlin/ee/carlrobert/codegpt/util/coroutines/CoroutineDispatchers.kt
+++ src/main/kotlin/ee/carlrobert/codegpt/util/coroutines/CoroutineDispatchers.kt
@@ -1,5 +1,6 @@
 package ee.carlrobert.codegpt.util.coroutines
 
+import com.intellij.openapi.application.ModalityState
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainCoroutineDispatcher
@@ -12,4 +13,20 @@ class CoroutineDispatchers {
     fun unconfined(): CoroutineDispatcher = Dispatchers.Unconfined
 
     fun io(): CoroutineDispatcher = Dispatchers.IO
+
+    fun edtNonModal(): CoroutineDispatcher = EdtDispatchers.NonModal
+
+    fun edtCurrent(): CoroutineDispatcher = EdtDispatchers.Current
+
+    fun edtDefault(): CoroutineDispatcher = EdtDispatchers.Default
+}
+
+object EdtDispatchers {
+    val NonModal = EdtCoroutineDispatcher(ModalityState.nonModal())
+    val Default = EdtCoroutineDispatcher()
+    val Current = EdtCoroutineDispatcher(ModalityState.current())
+
+    fun withModalityState(modalityState: ModalityState): CoroutineDispatcher {
+        return EdtCoroutineDispatcher(modalityState)
+    }
 }
